378e0066cb0e4df77fcf30ee49422746b9f41646
tryt007 <try.3y7n5n9n1@outlook.com>
2024-05-24 17:41:14 +0800

    Update data.js

--- test/data.js
+++ test/data.js
@@ -2,5 +2,8 @@ callbackBackupAgentApi({
   "URL1": {
     "j": "2eadd228eb24a5e038288a64e1611c8679e2831ff0418291adcda254ebb0e873cf0556d4529049e4fae6aa225a5f4b6b",
     "l": "8764a8b93de5c94719150ab08836ba5dfe03c262b8831efba9d4510aee948d9c25318304f3a6daf8a0fb5339c0399035"
-  }
+  },
+  "URL2": {
+    "k": "123"
+  }   
 });
